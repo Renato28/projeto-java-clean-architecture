df8a42dad3d85c18c99809d983e23ced170276e2
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-14 21:26:16 -0300

    criando o projeto

---
+++ src/main/java/br/com/github/renato28/projetpjavacleanarchitecture/ProjetpJavaCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.renato28.projetpjavacleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetpJavaCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetpJavaCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/github/renato28/projetpjavacleanarchitecture/ProjetpJavaCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.renato28.projetpjavacleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetpJavaCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
